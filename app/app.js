c5ed2b95cd738b4b425f94634d2899840841adc8
Edgar Montano <em2532@nyu.edu>
2016-08-18 16:52:41 -0400

    added models for schemas

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express =     require('express');
+var path =        require('path');
+var favicon =     require('serve-favicon');
+var logger =      require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser =  require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes =      require('./routes/index');
+var users =       require('./routes/users');
+
+//mongoose database connect
+var mongoose =    require('mongoose');
+mongoose.connect('mongodb://localhost/app');
 
 var app = express();
 
