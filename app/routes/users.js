e72113ed49ffef07a2b5bdd78f4950c5a50610e2
Edgar Montano <em2532@nyu.edu>
2016-08-19 14:10:26 -0400

    added stuff but not completely

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose'),
+      Schema = mongoose.Schema,
+passportLocalMongoose = require('passport-local-mongoose');
+
+var passport      =     require('passport');
+var LocalStrategy =     require('passport-local').Strategy;
+var flash         =     require('connect-flash');
+//var db = require('db.js');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/login',function(req,res){
+  res.render('login.hbs', {message: req.flash('Login')});
+});
+
+router.post('/login', passport.authenticate('local', { layout:'',
+                                                       successRedirect: '/',
+                                                       failureRedirect: '/login?failure=true',
+                                                       failureFlash: "login failed!",
+                                                       successFlash: "login successful!"}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/signup',function(req,res){
+  res.render('signup.hbs', {message: req.flash('Signup')});
 });
 
 module.exports = router;
