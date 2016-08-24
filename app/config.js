524b694fbccc34c261436530fc69bf377060b2cc
SharpAceX <sharpacex@gmail.com>
2016-08-23 20:19:50 -0400

    User schema almost complete

--- app/config.js
+++ app/config.js
@@ -16,4 +16,33 @@ config.passportSecret = 'passport_secret';
 //MongoDB
 config.mongooseUri = 'mongodb://localhost/app';
 
+//User Schema
+config.usernameMinLength = 3;
+config.usernameMaxLength = 32;
+
+config.passwordMinLength = 8;
+config.passwordMaxLength = 32;
+
+config.emailMinLength = 3;
+config.emailMaxLength = 64;
+
+config.nameMaxLength = 32;
+
+config.bioMaxLength = 500;
+
+config.topGamesLength = 5;
+
+//Platform-specific
+config.steamNameMinLength = 0;
+config.steamNameMaxLength = 0;
+
+config.xboxGamertagMinLength = 0;
+config.xboxGamertagMaxLength = 0;
+
+config.psnIdMinLength = 0;
+config.psnIdMaxLength = 0;
+
+config.twitchNameMinLength = 0;
+config.twitchNameMaxLength = 0;
+
 module.exports = config;
--- app/models/user.js
+++ app/models/user.js
@@ -1,34 +1,113 @@
 var mongoose = require('mongoose');
-var passportLocalMongoose = require('passport-local-mongoose');
+var plugin = require('passport-local-mongoose');
+var config = require('../config');
 
 var User = new mongoose.Schema({
-    username:    {type:String,  required:true, unique:true}, //3 min 32 max
-    password:    {type:String,  required:true}, //8 to 32 min max
-    salt:        {type:String,  required:true},
-    email:       {type:String,  required:true, unique:true}, //3 min 64 max
-    gender:      {type:Number, required:true},
-    birthday:    {type:Date,    required:true},
-    //location: country, state, city
-    country:     {type:String,  required:true},
-    state:       {type:String,  required:true},
-    city:        {type:String,  required:true},
-    first_name:  {type:String,  required:false}, //max 32
-    last_name:   {type:String,  required:false}, //max 32
-    //[profile]: bio, top_games, profile_pic, one_ups, images
-    bio:         {type:String,  required:false}, //max 500
-    top_games:   {type:String,  required:false},//max 5 array of ids 
-    profile_pic: {type:String,  required:false},
-    one_ups:     {type:Number,  default:0},
-    //[images]:  {type:String,  required:false},
-    steam:       {type:String,  required:false},
-    xbox:        {type:String,  required:false},
-    playstation: {type:String,  required:false},
-    twitch:      {type:String,  required:false},
-    coins:       {type:Number,  default:0},
-    timestamp:   {type:Date,    default:Date.now},
-    deleted:     {type:Boolean, default:0}
+    username: {
+        type: String,
+        required: true,
+        minlength: config.usernameMinLength,
+        maxlength: config.usernameMaxLength,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: config.passwordMinLength,
+        maxlength: config.passwordMaxLength
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: config.emailMinLength,
+        maxlength: config.emailMaxLength,
+        unique: true
+    },
+    gender: {
+        type: Number,
+        required: true
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    location: {
+        country: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        required: true
+    },
+    first_name: {
+        type: String,
+        maxlength: config.nameMaxLength
+    },
+    last_name: {
+        type: String,
+        maxlength: config.nameMaxLength
+    },
+    profile: {
+        bio: {
+            type: String,
+            maxlength: config.bioMaxLength
+        },
+        top_games: [{
+            type: String,
+            validate: [arrayLimit, '{PATH} exceeds the limit of ' + config.topGamesLength]
+        }],
+        profile_pic: {
+            type: String
+        },
+        one_ups: {
+            type: Number,
+            default: 0
+        },
+        images: [{
+            type: String
+        }],
+    },
+    steam: {
+        type: String
+    },
+    xbox: {
+        type: String
+    },
+    playstation: {
+        type: String
+    },
+    twitch: {
+        type: String
+    },
+    coins: {
+        type: Number,
+        default: 0
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    deleted: {
+        type: Boolean,
+        default: 0
+    }
 });
 
-User.plugin(passportLocalMongoose);
+function arrayLimit(val) {
+    return val.length <= config.topGamesLength;
+}
+
+User.plugin(plugin);
 
 module.exports = mongoose.model('User', User);
