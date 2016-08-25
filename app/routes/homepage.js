5f0ed2701ac165f9299dfb96d583dfbee92fbdd9
SharpAceX <sharpacex@gmail.com>
2016-08-24 23:12:19 -0400

    Tagline footer added

--- app/routes/homepage.js
+++ app/routes/homepage.js
@@ -7,12 +7,12 @@ router.get('/', function(req, res, next) {
         layout: 'primary',
         file: 'homepage',
         user: req.user,
-        live_profile_users: getUsers()
+        live_profile_users: getLiveProfileUsers()
     });
 });
 
-function getUsers() {
-    var fakeUsers = [{
+function getLiveProfileUsers() {
+    var liveProfileUsers = [{
         profile_pic: "http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/64/-profile-filled.png",
         username: "SharpAceX",
         oneUps: 186,
@@ -33,7 +33,7 @@ function getUsers() {
         username: "Anteneh",
         oneUps: 64,
     }];
-    return fakeUsers;
+    return liveProfileUsers;
 }
 
 module.exports = router;
