65f7264a39889a8b97262f16b764b342b57273c9
SharpAceX <sharpacex@gmail.com>
2016-08-26 02:08:41 -0400

    Minor bug fixed, code organized a bit more

--- app/routes/homepage.js
+++ app/routes/homepage.js
@@ -3,22 +3,15 @@ var router = express.Router();
 
 var User = require('../models/user');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     var database = true; //true loads users from database, false uses preset users below
 
-    if (database){
-        User.find().sort({'one_up_count': -1}).limit(5).exec(function(err, users) {
-            users = addPlatforms(users);
-            res.render('homepage', {
-                title: 'wasdWithMe - Connect with gamers.',
-                layout: 'primary',
-                file: 'homepage',
-                user: req.user,
-                live_profile_users: users
-            });
-        });
-    } else {
-        var users = getPresetUsers();
+    if (!database) {
+        loadPresetUsers(req, res);
+        return;
+    }
+
+    User.find().sort({'one_up_count': -1}).limit(5).exec(function (err, users) {
         users = addPlatforms(users);
         res.render('homepage', {
             title: 'wasdWithMe - Connect with gamers.',
@@ -27,35 +20,44 @@ router.get('/', function(req, res, next) {
             user: req.user,
             live_profile_users: users
         });
-    }
+    });
 });
 
-function addPlatforms(users){
+function loadPresetUsers(req, res) {
+     var users = [{
+        display_name: "SharpAceX",
+        one_up_count: 186,
+    }, {
+        display_name: "Alan",
+        one_up_count: 153,
+    }, {
+        display_name: "JohnCena",
+        one_up_count: 135,
+    }, {
+        display_name: "Edgar",
+        one_up_count: 89,
+    }, {
+        display_name: "Anteneh",
+        one_up_count: 64,
+    }];
+    users = addPlatforms(users);
+    res.render('homepage', {
+        title: 'wasdWithMe - Connect with gamers.',
+        layout: 'primary',
+        file: 'homepage',
+        user: req.user,
+        live_profile_users: users
+    });
+}
+
+function addPlatforms(users) {
     var allPlatforms = ["Steam", "Xbox", "Playstation", "Twitch"];
     for (var i = 0; i < users.length; i++) {
         //I won't even bother commenting this mess, it is only temporary
-        users[i].platforms = allPlatforms.sort( function() { return 0.5 - Math.random() }).slice(0, Math.floor(Math.random() * allPlatforms.length) + 1);
+        users[i].platforms = allPlatforms.sort(function () {
+            return 0.5 - Math.random()
+        }).slice(0, Math.floor(Math.random() * allPlatforms.length) + 1);
     }
-    return users;
-}
-
-function getPresetUsers() {
-    var users = [{
-        display_name: "SharpAceX",
-        oneUps: 186,
-    }, {
-        display_name: "Alan",
-        oneUps: 153,
-    }, {
-        display_name: "JohnCena",
-        oneUps: 135,
-    }, {
-        display_name: "Edgar",
-        oneUps: 89,
-    }, {
-        display_name: "Anteneh",
-        oneUps: 64,
-    }];
     return users;
 }
 
