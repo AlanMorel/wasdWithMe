9452f5fdab2756bfef978419cf39fb172250fd32
SharpAceX <sharpacex@gmail.com>
2016-08-26 22:46:50 -0400

    Online/offline status added

--- app/routes/user.js
+++ app/routes/user.js
@@ -38,6 +38,7 @@ function addTemporaryInfo(owner){
     owner.last_name = "Morel";
     //randomly generated bio courtesy of http://www.generatorland.com/glgenerator.aspx?id=124&rlx=y
     owner.bio = "Spent the 80's getting my feet wet with childrens books in Africa. Spent 2001-2004 supervising the production of pond scum in Orlando, FL. At the moment I'm marketing puppets in Ocean City, NJ. Spent college summers donating mosquito repellent in Pensacola, FL. Have some experience exporting human growth hormone for the government. Spent college summers buying and selling rocking horses in the aftermarket."
+    owner.online_status = "online"; //"offline" to test offline status
 }
 
 function isOwner(user, owner){
