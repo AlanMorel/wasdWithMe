396c9a933564bd948a78da77098f83408c63e1fb
SharpAceX <sharpacex@gmail.com>
2016-09-09 19:11:05 -0400

    Selecting an image previews it, setting up actual file uploading

--- app/public/javascript/user.js
+++ app/public/javascript/user.js
@@ -2,3 +2,18 @@ function addAnother() {
     var input = '<input type="text" name="games" class="input-game">';
     document.querySelector(".games-list").innerHTML += input;
 }
+
+document.querySelector(".pic").onclick = function() {
+    var upload = document.querySelector(".image-upload");
+    upload.click();
+};
+
+document.querySelector(".image-upload").onchange = function () {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        document.querySelector(".profile-pic").src = e.target.result;
+    };
+
+    reader.readAsDataURL(this.files[0]);
+};
