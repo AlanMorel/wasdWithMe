19fa64461a81c57dff1fb2b34ad7b2b874518669
SharpAceX <sharpacex@gmail.com>
2016-09-20 21:18:58 -0400

    Cleaned, commented, simplified and made edit.js more efficient

--- app/public/javascript/edit.js
+++ app/public/javascript/edit.js
@@ -1,55 +1,50 @@
 var currentInput = null;
+var subbox = document.querySelector("span.sub-results-box");
 
-function addOnKeyUps(){
-    var inputs = document.querySelectorAll(".input-game");
-    for (var i = 0; i < inputs.length; ++i) {
-        inputs[i].onkeyup = function(){
-            currentInput = this;
-            var request = new XMLHttpRequest();
+//Searches for games whenever a key is pressed in any text input
+var searchGame = function(){
+    currentInput = this;
+    var request = new XMLHttpRequest();
 
-            var subbox = document.querySelector("span.sub-results-box");
-            this.parentNode.insertBefore(subbox, this.nextSibling);
+    request.onreadystatechange = function() {
+        if (request.readyState != 4 || request.status != 200 || request.responseText.length < 1){
+            //If something went wrong or no results returned, hide box
+            subbox.style.visibility = "hidden";
+            subbox.style.display = "none";
+            return;
+        }
 
-            request.onreadystatechange = function() {
-                if (request.readyState != 4 || request.status != 200 || request.responseText.length < 1){
-                    //If something went wrong or no results returned, hide box
-                    subbox.style.visibility = "hidden";
-                    subbox.style.display = "none";
-                    return;
-                }
+        //Move the result box to below this input
+        currentInput.parentNode.insertBefore(subbox, currentInput.nextSibling);
 
-                //Display results to the user
-                subbox.innerHTML = request.responseText;
-                subbox.style.visibility = "visible";
-                subbox.style.display = "block";
+        //Display results to the user
+        subbox.innerHTML = request.responseText;
+        subbox.style.visibility = "visible";
+        subbox.style.display = "block";
 
-                //Give each game an onclick event
-                var children = subbox.childNodes;
-                for(var child in children) {
-                    children[child].onclick = selectGame;
-                }
-            };
+        //Give each game an onclick event
+        var children = subbox.childNodes;
+        for(var child in children) {
+            children[child].onclick = selectGame;
+        }
+    };
 
-            request.open("GET", "/api/search?q=" + this.value, true);
-            request.send();
-        };
-    }
-}
+    request.open("GET", "/api/search?q=" + this.value, true);
+    request.send();
+};
 
+//Selects a game whenever it is clicked
 var selectGame = function(){
-    //Set the text input to the clicked game
+    //Set the text input to the clicked game's name
     var name = this.querySelector(".name").innerHTML;
     currentInput.value = name;
     currentInput.readOnly = true;
 
     //Hide the box since game selection was made
-    var subbox = document.querySelector("span.sub-results-box");
     subbox.style.visibility = "hidden";
     subbox.style.display = "none";
     return false;
 };
-
-addOnKeyUps();
 
 //Add a new game input when the button is clicked
 document.querySelector(".add-another").onclick = function() {
@@ -57,9 +52,9 @@ document.querySelector(".add-another").onclick = function() {
     input.type = "text";
     input.className = "input-game";
     input.name = "games";
-    document.querySelector(".games-list").appendChild(input);
+    input.onkeyup = searchGame;
 
-    addOnKeyUps();
+    document.querySelector(".games-list").appendChild(input);
 };
 
 //Start the upload process when the pic is clicked
