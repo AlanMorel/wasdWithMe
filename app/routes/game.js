ab29d3b5dafacd0f7a12f0cfa01dd018149d2995
SharpAceX <sharpacex@gmail.com>
2016-09-25 23:21:09 -0400

    Fixed banner, moved rating logic to own function

--- app/routes/game.js
+++ app/routes/game.js
@@ -58,23 +58,8 @@ function displayGame(req, res, query, game){
     }
 
     var release = getReleaseDate(game.release_date);
-
     var banner = getBanner(game.screenshots);
-
-    var rating = {};
-
-    if (game.rating && game.rating > 0){
-        //Set the rating as an int with a corresponding color
-        var number = Math.ceil(game.rating);
-        rating.number = number;
-        if (number > 80){
-            rating.color = "limegreen";
-        } else if (number > 60) {
-            rating.color = "goldenrod";
-        } else {
-            rating.color = "firebrick";
-        }
-    }
+    var rating = getRating(game.rating);
 
     return res.render('game', {
         title: game.display_name,
@@ -101,7 +86,23 @@ function getReleaseDate(releaseDate){
 function getBanner(screenshots){
     if (screenshots){
         //select a random screenshot as the game banner
-        banner = screenshots[Math.floor(Math.random() * screenshots.length)];
+        return screenshots[Math.floor(Math.random() * screenshots.length)];
+    }
+}
+
+function getRating(rating){
+    if (rating && rating > 0){
+        //Set the rating as an int with a corresponding color
+        var ratingObj = {};
+        ratingObj.number = Math.ceil(rating);
+        if (ratingObj.number > 80){
+            ratingObj.color = "limegreen";
+        } else if (ratingObj.number > 60) {
+            ratingObj.color = "goldenrod";
+        } else {
+            ratingObj.color = "firebrick";
+        }
+        return ratingObj;
     }
 }
 
