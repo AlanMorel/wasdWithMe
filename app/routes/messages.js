99ee9831c301840baf32cdbf2bb028be31f35605
SharpAceX <sharpacex@gmail.com>
2016-10-28 19:55:59 -0400

    Server checks for user in db before displaying messages

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var Logger = require('../utility/logger');
+var User   = require('../models/user');
 
 //handles get request for all messages
 router.get('/', function (req, res, next) {
+
+    //display all messages here
+
     res.render('message', {
         title: 'All Messages',
         layout: 'primary',
@@ -17,18 +21,38 @@ router.get('/', function (req, res, next) {
 //handles GET requests to /message/username
 router.get('/:username', function (req, res, next) {
 
-    //check if user is logged in
+    var username = req.params.username.toLowerCase();
 
-    var to = req.params.username;
+    User.findByUsername(username, true, function(err, owner) {
 
-    res.render('message', {
-        title: 'Message with ' + to,
-        layout: 'primary',
-        file: 'message',
-        user: req.user,
-        js: ["/socket.io/socket.io", "/javascript/messages"],
-        to: to
+        //if error or user not found, return userNotFound
+        if (err || !owner){
+            return userNotFound(res, req.user, username);
+        }
+
+        //addTemporaryInfo(owner);
+
+        res.render('message', {
+            title: 'Messages with ' + username,
+            layout: 'primary',
+            file: 'message',
+            user: req.user,
+            js: ["/socket.io/socket.io", "/javascript/messages"],
+            to: username
+        });
     });
 });
+
+//renders 404 page whenever a user was not found
+function userNotFound(res, user, username){
+    res.status(404);
+    res.render('404', {
+        title: 'User not found!',
+        layout: 'primary',
+        file: '404',
+        user: user,
+        message: "We could not find any user named '" + username + "'."
+    });
+}
 
 module.exports = router;
